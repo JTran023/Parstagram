5f984b0be7aea1c0afabf6cde89a144b91ce4c8c
JTran013 <jtran023@fiu.edu>
2021-10-08 13:51:09 -0400

    Create Action on CameraViewController
    onSubmitButton

--- Parstagram/CameraViewController.swift
+++ Parstagram/CameraViewController.swift
@@ -13,13 +13,14 @@ class CameraViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var commentField: UITextField!
     
-    
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onSubmitButton(_ sender: Any) {
+        
     }
     
 
