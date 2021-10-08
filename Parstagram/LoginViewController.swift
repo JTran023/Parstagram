6ff8833a8d74d4ee847e305ad6c72066787e547e
JTran013 <jtran023@fiu.edu>
2021-10-08 10:39:13 -0400

    Create Actions
    onSignIn
    onSignOut

--- Parstagram/LoginViewController.swift
+++ Parstagram/LoginViewController.swift
@@ -12,7 +12,10 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var usernameField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
     
-    
+    @IBAction func onSignIn(_ sender: Any) {
+    }
+    @IBAction func onSignUp(_ sender: Any) {
+    }
     
     
     override func viewDidLoad() {
